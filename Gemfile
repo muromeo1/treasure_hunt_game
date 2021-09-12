709362f4d2c226b2273edced61a172b1cb44bb46
Murilo Romeo <muriloh65@gmail.com>
2021-09-12 16:06:55 -0300

    adds configuration and initial code

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.0'
+
+gem 'bcrypt', '~> 3.1.7'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'rack-cors'
+gem 'rails', '~> 6.1.4'
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+
+group :development, :test do
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'faker'
+  gem 'pry'
+  gem 'reek'
+  gem 'rubocop'
+end
---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+require 'action_view/railtie'
+require 'rails/test_unit/railtie'
+
+Bundler.require(*Rails.groups)
+
+module TreasureHuntGame
+  class Application < Rails::Application
+    config.load_defaults 6.1
+    config.api_only = true
+  end
+end
